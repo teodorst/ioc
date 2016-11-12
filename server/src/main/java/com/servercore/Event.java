6c54c4611169ad7ee693b57119127cd0f1149396
Andrei Vlad Postoaca <vlad.postoaca@codaintelligence.com>
2016-11-13 00:50:49 +0200

    Integrated support for mysql database calls.

--- server/src/main/java/com/servercore/Event.java
+++ server/src/main/java/com/servercore/Event.java
@@ -5,18 +5,34 @@ import java.util.logging.Logger;
 
 public class Event {
 
-	private String id;
+	private long id;
 	private String name;
+	private String location;
+	private String date;
 	private List<User> participants;
 	
 	private final static Logger LOGGER = Logger.getLogger(Event.class.getName());
 
-	public String getId() {
+	public Event(String name, String location, String date) {
+		this.name = name;
+		this.location = location;
+		this.date = date;
+	}
+	
+	public long getId() {
 		return id;
 	}
 	
 	public String getName() {
 		return name;
+	}
+	
+	public String getLocation() {
+		return location;
+	}
+	
+	public String getDate() {
+		return date;
 	}
 	
 	public List<User> getParticipants() {
--- server/src/main/java/com/servercore/ServerMain.java
+++ server/src/main/java/com/servercore/ServerMain.java
@@ -1,5 +1,9 @@
 package com.servercore;
 
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,7 +12,65 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 @SpringBootApplication
 public class ServerMain {
 
+	private final static Logger LOGGER = Logger.getLogger(User.class.getName());
+	private final static boolean TESTING = true;
+	
+	public static void dropTables(Statement statement) throws SQLException {
+		
+		String dropUsersTable = "drop table users;";
+		statement.executeUpdate(dropUsersTable);
+		
+		String dropEventsTable = "drop table events;";
+		statement.executeUpdate(dropEventsTable);
+	}
+	
+	public static void initDatabase() {
+		
+		Connection connection = null;
+		Statement statement = null;
+		try{
+			LOGGER.log(Level.INFO, "Connecting to the database.");
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/evshare", "root", "test");
+			statement = connection.createStatement();
+			
+			//TODO: remove after testing is done
+			if (TESTING)
+				dropTables(statement);
+		
+			LOGGER.log(Level.INFO, "Creating the users table.");
+			String createTableUsers = "create table users " +
+						"(email VARCHAR(255), " +
+						" firstName VARCHAR(255), " + 
+						" lastName VARCHAR(255), " + 
+						" password VARCHAR(255), " + 
+						" PRIMARY KEY (email))";		
+			statement.executeUpdate(createTableUsers);
+			
+			LOGGER.log(Level.INFO, "Creating the events table.");
+			String createTableEvents = "create table events " +
+						"(id INTEGER not NULL, " +
+						" name VARCHAR(255), " + 
+						" location VARCHAR(255), " + 
+						" date DATETIME, " + 
+						" PRIMARY KEY (id))";		
+			statement.executeUpdate(createTableEvents);
+			
+			connection.close();
+		}
+		catch(Exception e)
+		{ 
+			e.printStackTrace();
+		}
+		finally {
+			
+		}
+	}
+
 	public static void main(String[] args) {
+		
+		initDatabase();
 		SpringApplication.run(ServerMain.class, args);
 		
 		ApplicationContext ctx = 

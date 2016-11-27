603596ed90f8b5ce9944598d611da2930eb3b336
Andra <andradenis.ionescu@gmail.com>
2016-11-27 21:59:13 +0200

    WEB-1 Setup Angular

---
+++ web/app/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular
+	.module('app', [
+		'ngRoute',
+		'ngResource',
+		'myApp.version',
+		'ds.clock'
+	]);
+
---
+++ web/app/appController.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular
+	.module('app')
+	.controller('AppController', [
+		AppController]);
+
+function AppController() {
+	var vm = this;
+
+	_init();
+	function _init() {
+
+	}
+
+
+}

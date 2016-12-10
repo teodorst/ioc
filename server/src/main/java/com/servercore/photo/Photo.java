69a310ae50f47dd7903960ec201e0e5ec7c4fe32
Teodor Stefu <Teodorstefu@gmail.com>
2016-12-10 12:31:12 +0200

    Added photo table

--- server/src/main/java/com/servercore/photo/Photo.java
+++ server/src/main/java/com/servercore/photo/Photo.java
@@ -1,5 +1,51 @@
 package com.servercore.photo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PHOTO")
 public class Photo {
 	
+
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "PATH")
+	private String photoPath;
+	
+	@Column(name = "USERID")
+	private Long userId;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPhotoPath() {
+		return photoPath;
+	}
+
+	public void setPhotoPath(String photoPath) {
+		this.photoPath = photoPath;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	
 }
+
